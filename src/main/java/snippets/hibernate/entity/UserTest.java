5fb234252a69f0331eea7eb530a2b0120f9171ae
790013438 <790013438@qq.com>
2017-10-09 10:06:07 +0800

    使用自己写的配置util

--- src/main/java/snippets/hibernate/entity/UserTest.java
+++ src/main/java/snippets/hibernate/entity/UserTest.java
@@ -1,17 +1,27 @@
 package snippets.hibernate.entity;
 
-import org.hibernate.cfg.Configuration;
+import org.hibernate.Session;
 import org.junit.Test;
+
+import snippets.hibernate.util.HibernateUtil;
 
 public class UserTest {
 
     @Test
     public void testSave() {
-        new Configuration.configure();
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Test
     public void testGet() {
-        
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        session.getTransaction().commit();
+        session.close();
     }
 }
