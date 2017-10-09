8a9b41fc37a2f22fb7ad600f0e05d904bab6573e
790013438 <790013438@qq.com>
2017-10-09 09:39:05 +0800

    junit

---
+++ src/main/java/snippets/hibernate/entity/UserTest.java
@@ -0,0 +1,17 @@
+package snippets.hibernate.entity;
+
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+public class UserTest {
+
+    @Test
+    public void testSave() {
+        new Configuration.configure();
+    }
+
+    @Test
+    public void testGet() {
+        
+    }
+}
