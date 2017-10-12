833c90f440832d2a5058eddcd4774544dc398e48
790013438 <790013438@qq.com>
2017-10-12 10:38:18 +0800

    inherited entity，对super class 也要加入persistent.xml

---
+++ src/main/java/snippets/jee/jpa/Wife.java
@@ -0,0 +1,10 @@
+package snippets.jee.jpa;
+
+import javax.persistence.Entity;
+
+@Entity(name="tb_wife")
+public class Wife extends Person {
+
+    private static final long serialVersionUID = 1L;
+
+}
